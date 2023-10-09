de022549e1aee3234bb77ffdb39c9377ee593e0f
Сергей Ястребов <yastrebos@vk.com>
2023-10-09 13:29:48 +0300

    Add pagintor.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,6 @@
 # birthday/views.py 
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
@@ -33,10 +34,20 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 10)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 def delete_birthday(request, pk):
